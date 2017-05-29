81f078fd3e95d6ddcf99726f336c696987611443
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-29 18:24:20 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create(
+  first_name: "Jay",
+  last_name: "Gatsby",
+  email: "jay.gatsby@gmail.com",
+  password: "123456",
+)
+User.create(
+  first_name: "Hannibal",
+  last_name: "Lecter",
+  email: "hannibal.lecter@gmail.com",
+  password: "123456",
+)
+User.create(
+  first_name: "Scarlett",
+  last_name: "O'Hara",
+  email: "scarlett.o.hara@gmail.com",
+  password: "123456",
+)
+User.create(
+  first_name: "Dorian",
+  last_name: "Gray",
+  email: "dorian.gray@gmail.com",
+  password: "123456",
+)
+User.create(
+  first_name: "Big",
+  last_name: "Brother",
+  email: "big.brother1984@gmail.com",
+  password: "123456",
+)
+
+30.times do
+  Article.create(
+    title: Faker::Lorem.sentence(rand(2..10)).chomp('.'),
+    description: Faker::Lorem.paragraphs(1),
+    user_id: rand(1..5)
+  )
+end
+
+60.times do
+  Reading.create(
+    article_id: rand(1..30),
+    user_id: rand(1..5),
+  )
+end
+
+20.times do
+  Comment.create(
+    reading_id: rand(1..60),
+    title: Faker::Lorem.sentence(rand(1..4)).chomp('.'),
+    content: Faker::Lorem.paragraphs(1),
+  )
+end
+
+20.times do
+  Following.create(
+    author_id: rand(1..5),
+    follower_id: rand(1..5)
+  )
+end
