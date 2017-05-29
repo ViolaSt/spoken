a40b857817b0abdb6356b6b773d3c38358170a62
Viola Stoeckli <violastoeckli@hotmail.com>
2017-05-29 17:38:59 +0100

    Add Readings Controller

---
+++ app/controllers/readings_controller.rb
@@ -0,0 +1,25 @@
+class ReadingsController < ApplicationController
+  def index
+  end
+
+  def create_like
+  end
+
+  def destroy_like
+  end
+
+  def create_recommendation
+  end
+
+  def edit_recommendation
+  end
+
+  def update_recommendation
+  end
+
+  def update_recommendation
+  end
+
+  def destroy_recommendation
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'readings/index'
+
+  get 'readings/create_like'
+
+  get 'readings/destroy_like'
+
+  get 'readings/create_recommendation'
+
+  get 'readings/edit_recommendation'
+
+  get 'readings/update_recommendation'
+
+  get 'readings/destroy_recommendation'
+
   resources :articles
   devise_for :users
   root to: 'pages#home'
