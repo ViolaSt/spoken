f6fdf59793412d3b7ef98f4ffd2e38b48bf8e0b8
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-30 11:29:52 +0100

    changed authentication rules for articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :authenticate_user!, only: [ :index ]
   # GET /articles
   # GET /articles.json
   def index
