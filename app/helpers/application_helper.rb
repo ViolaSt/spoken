2dcedefd6e27d8866375fd399f809471075aff06
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-31 16:00:06 +0100

    hover image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,9 @@ module ApplicationHelper
     end
   end
 
+
+
   def user_image_path(user)
-    if user.photo?
-      cl_image_path user.photo, height: 150, width: 150, crop: "thumb", gravity: "face"
-    else
-      ""
-    end
+      cl_image_path user.photo, height: 150, width: 150, crop: "thumb", gravity: "face" || image_path("profile_avatar")
   end
 end
