7dc1fc22280e6293543721c54a287e1e0e8ea7c9
Viola Stoeckli <violastoeckli@hotmail.com>
2017-05-31 11:47:20 +0100

    Added Reading policies for pundit

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -7,24 +7,28 @@ before_action :set_reading, only: [:like, :unlike, :recommend, :unrecommend]
 
   def like
     @reading.like = true
+    authorize @reading
     @reading.save
     redirect_to @article
   end
 
   def unlike
     @reading.like = false
+    authorize @reading
     @reading.save
     redirect_to @article
   end
 
   def recommend
     @reading.recommended = true
+    authorize @reading
     @reading.save
     redirect_to @article
   end
 
   def unrecommend
     @reading.recommended = false
+    authorize @reading
     @reading.save
     redirect_to @article
   end
