a3db8ac6bc5860ddb4d890a77e81b052cb48e090
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-06-08 01:08:03 +0100

    article creation works on localhost

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :reading
 
-  validates :title, :content , presence: true
+  validates :content , presence: true
   validates :content, length: { minimum: 20 }
 end
